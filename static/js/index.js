5a9c0a83b136b15225bf4b866df1ac43dfff056f
aladonmac <aladonwlh@stu.pku.edu.cn>
2026-07-16 21:52:51 +0800

    init template from POPE and RaC

--- static/js/index.js
+++ static/js/index.js
@@ -1,100 +1,25 @@
-window.HELP_IMPROVE_VIDEOJS = false;
+function toggleExpandable(panelId, toggleButtonSelector, toggleTextSelector) {
+    const panel = document.getElementById(panelId);
+    const toggleButton = document.querySelector(toggleButtonSelector);
+    const toggleText = document.querySelector(toggleTextSelector);
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
-
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
-
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
-
-
-$(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
-    });
-
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
+    if (!panel || !toggleButton || !toggleText) {
+        return;
     }
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    // preloadInterpolationImages();
-
-    // $('#interpolation-slider').on('input', function(event) {
-    //   setInterpolationImage(this.value);
-    // });
-    // setInterpolationImage(0);
-    // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
-
-    bulmaSlider.attach();
-
-})
-
-// Function to toggle abstract expansion
-function toggleAbstract() {
-    const abstractFull = document.getElementById('abstractFull');
-    const toggleButton = document.querySelector('.abstract-toggle');
-    const toggleText = document.querySelector('.toggle-text');
-    const toggleIcon = document.querySelector('.toggle-icon');
-    
-    if (abstractFull.classList.contains('expanded')) {
-        // Collapse
-        abstractFull.classList.remove('expanded');
+    if (panel.classList.contains('expanded')) {
+        panel.classList.remove('expanded');
         toggleButton.classList.remove('expanded');
-        toggleText.textContent = 'Click to Read More';
+        toggleText.textContent = 'Click to View More';
     } else {
-        // Expand
-        abstractFull.classList.add('expanded');
+        panel.classList.add('expanded');
         toggleButton.classList.add('expanded');
-        toggleText.textContent = 'Click to Read Less';
+        toggleText.textContent = 'Click to View Less';
     }
+}
+
+function toggleRollouts() {
+    toggleExpandable('rolloutsPanel', '.expandable-toggle', '.rollout-toggle-text');
 }
 
 // =================================
@@ -103,7 +28,7 @@ function toggleAbstract() {
 
 class NavigationManager {
     constructor() {
-        this.sections = ['introduction', 'data-collection', 'results', 'evaluation', 'citation'];
+        this.sections = ['introduction', 'abstract', 'method', 'results', 'rollouts', 'citation'];
         this.navLinks = {
             desktop: document.querySelectorAll('.nav-link'),
             mobile: document.querySelectorAll('.nav-link-mobile')
